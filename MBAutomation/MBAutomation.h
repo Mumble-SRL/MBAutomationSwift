a6053a0bf891cbaa8d036e98705e622694293d41
lorenzOliveto <lorenzo.oliveto@gmail.com>
2020-06-30 10:57:34 +0200

    Initial Commit

---
+++ MBAutomation/MBAutomation.h
@@ -0,0 +1,19 @@
+//
+//  MBAutomation.h
+//  MBAutomation
+//
+//  Created by Lorenzo Oliveto on 30/06/2020.
+//  Copyright © 2020 Lorenzo Oliveto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MBAutomation.
+FOUNDATION_EXPORT double MBAutomationVersionNumber;
+
+//! Project version string for MBAutomation.
+FOUNDATION_EXPORT const unsigned char MBAutomationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MBAutomation/PublicHeader.h>
+
+
