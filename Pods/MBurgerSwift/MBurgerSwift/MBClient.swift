ebcf019f9a38a16fb0d034a41bd15b5f3fcda77c
lorenzOliveto <lorenzo.oliveto@gmail.com>
2021-03-31 09:42:34 +0200

    Updated pods

--- Pods/MBurgerSwift/MBurgerSwift/MBClient.swift
+++ Pods/MBurgerSwift/MBurgerSwift/MBClient.swift
@@ -15,6 +15,8 @@ import MBNetworkingSwift
 
 public final class MBClient {
     
+    // MARK: - Project
+    
     /// Retrieve the informations about the project.
     /// - Parameters:
     ///   - includingContracts: If `true` contracts are included in the project, `false` by default.
@@ -44,6 +46,8 @@ public final class MBClient {
             failure(error)
         })
     }
+    
+    // MARK: - Blocks
     
     /// Retrieve the blocks of the project.
     /// - Parameters:
@@ -139,10 +143,12 @@ public final class MBClient {
         })
     }
     
+    // MARK: - Sections
+
     /// Retrieve the sections of the block with the specified id.
     /// - Parameters:
     ///   - blockId: The `id` of the block.
-    ///   - parameters: An optional array of `MBParameter` used to sort, `nil` by default.
+    ///   - parameters: An optional array of `MBParameter`, `nil` by default.
     ///   - elements: If `true` the informations about the elements in the sections of the blocks are included in the response, `false` by default.
     ///   - success: A block that will be called when the request ends successfully. This block has no return value and takes two arguments.
     ///   - sections: An array of `MBSection` returned by the api.
@@ -191,6 +197,7 @@ public final class MBClient {
     /// Retrieve the section with the specified id.
     /// - Parameters:
     ///   - sectionId: The `id` of the section.
+    ///   - parameters: An optional array of `MBParameter`, `nil` by default.
     ///   - elements: If `true` the informations about the elements in the sections of the blocks are included in the response, `false` by default.
     ///   - success: A block that will be called when the request ends successfully. This block has no return value and takes one argument.
     ///   - section: A `MBSection` returned by the api.
@@ -230,6 +237,52 @@ public final class MBClient {
         })
     }
     
+    /// Retrieve the section with the specified slug.
+    /// - Parameters:
+    ///   - slug: The `slug` of the section.
+    ///   - parameters: An optional array of `MBParameter`, `nil` by default.
+    ///   - elements: If `true` the informations about the elements in the sections of the blocks are included in the response, `false` by default.
+    ///   - success: A block that will be called when the request ends successfully. This block has no return value and takes one argument.
+    ///   - section: A `MBSection` returned by the api.
+    ///   - failure: A block that will be called when the request ends incorrectly. This block has no return value and takes one argument.
+    ///   - error: The error describing the error that occurred.
+    
+    public static func getSection(withSlug slug: String,
+                                  parameters: [MBParameter]? = nil,
+                                  elements: Bool = false,
+                                  success: @escaping (_ section: MBSection) -> Void,
+                                  failure: @escaping (_ error: Error) -> Void) {
+        var apiParameters: Parameters = [:]
+        
+        apiParameters["use_slug"] = "true"
+        if elements {
+            apiParameters["include"] = "elements"
+        }
+        
+        if let parameters = parameters {
+            for parameter in parameters {
+                apiParameters.merge(parameter.parameterRepresentation(), uniquingKeysWith: {$1})
+            }
+        }
+
+        let apiName = String(format: "sections/%@", slug)
+        MBApiManager.request(withToken: MBManager.shared.apiToken,
+                             locale: MBManager.shared.localeString,
+                             apiName: apiName,
+                             method: .get,
+                             parameters: apiParameters,
+                             development: MBManager.shared.development,
+                             encoding: URLParameterEncoder.queryItems,
+                             success: { response in
+                                let section = MBSection(response)
+                                success(section)
+        }, failure: { error in
+            failure(error)
+        })
+    }
+
+    // MARK: - Elements
+
     /// Retrieve the elements of the section with the specified id.
     /// - Parameters:
     ///   - sectionId: The `id` of the section.
@@ -260,5 +313,60 @@ public final class MBClient {
         }, failure: { error in
             failure(error)
         })
+    }
+    
+    // MARK: - Media
+    
+    /// Retrieve the media of the project with the specified id.
+    /// - Parameters:
+    ///   - id: The `id` of the media.
+    ///   - success: A block that will be called when the request ends successfully. This block has no return value and takes one argument.
+    ///   - media: The media requested.
+    ///   - failure: A block that will be called when the request ends incorrectly. This block has no return value and takes one argument.
+    ///   - error: The error describing the error that occurred.
+    public static func getMedia(withId id: Int,
+                                success: @escaping (_ media: MBMedia) -> Void,
+                                failure: @escaping (_ error: Error) -> Void) {
+        let apiName = String(format: "media/%d", id)
+        MBApiManager.request(withToken: MBManager.shared.apiToken,
+                             locale: MBManager.shared.localeString,
+                             apiName: apiName,
+                             method: .get,
+                             development: MBManager.shared.development,
+                             encoding: URLParameterEncoder.queryItems,
+                             success: { response in
+                                let media = MBMedia(dictionary: response)
+                                success(media)
+        }, failure: { error in
+            failure(error)
+        })
+    }
+    
+    /// Retrieve all the media of the project.
+    /// - Parameters:
+    ///   - success: A block that will be called when the request ends successfully. This block has no return value and takes one argument.
+    ///   - media: All the media that are present in the project.
+    ///   - failure: A block that will be called when the request ends incorrectly. This block has no return value and takes one argument.
+    ///   - error: The error describing the error that occurred.
+    public static func getAllMedia(success: @escaping (_ media: [MBMedia]) -> Void,
+                                   failure: @escaping (_ error: Error) -> Void) {
+        let apiName = "media"
+        MBApiManager.request(withToken: MBManager.shared.apiToken,
+                             locale: MBManager.shared.localeString,
+                             apiName: apiName,
+                             method: .get,
+                             development: MBManager.shared.development,
+                             encoding: URLParameterEncoder.queryItems,
+                             success: { response in
+                                guard let body = response["body"] as? [[String: Any]] else {
+                                    success([])
+                                    return
+                                }
+                                let media = body.map({ MBMedia(dictionary: $0) })
+                                success(media)
+        }, failure: { error in
+            failure(error)
+        })
+
     }
 }
